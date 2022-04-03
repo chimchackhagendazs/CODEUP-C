8ebdea4e1efee359a5c2f4b0e2aa7fb15a1999d3
PepsiZeroPpa <chinchackseojun@gmail.com>
2022-04-03 16:29:32 +0900

    CodeUp1045

---
+++ CodeUp1045.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+ 
+ 
+int main (void)
+{
+    int num1, num2;
+    scanf("%d %d",&num1, &num2);
+    printf("%d\n", num1+num2);
+    printf("%d\n", num1-num2);
+    printf("%d\n", num1*num2);
+    printf("%d\n", num1/num2);
+    printf("%d\n", num1%num2);
+    printf("%.02f", (float)num1/(float)num2);
+    
+    return 0;
+}
+
