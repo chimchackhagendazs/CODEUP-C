8876e00096dbe53212ffd241fe1e624b4f92b483
PepsiZeroPpa <chinchackseojun@gmail.com>
2022-04-03 17:03:04 +0900

    CodeUp1068

---
+++ CodeUp1068.c
@@ -0,0 +1,13 @@
+int main(void)
+{
+    int score;
+    scanf("%d",&score);
+    
+    if(score >= 90) printf("A");
+    else if(score >=70) printf("B");
+    else if(score >=40) printf("C");
+    else printf("D");
+    
+    return 0;
+}
+
