a9e3ef0e3db0ceed7364731b9ef25cc9ef7832dc
PepsiZeroPpa <chinchackseojun@gmail.com>
2022-04-03 17:02:50 +0900

    CodeUp1065

---
+++ CodeUp1065.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+ 
+ 
+int main (void)
+{
+    int num1,num2,num3;
+ 
+    
+    scanf("%d %d %d", &num1 , &num2, &num3);
+    if( (num1%2) == 0) printf("%d\n",num1);
+    if( (num2%2) == 0) printf("%d\n",num2);
+    if( (num3%2) == 0) printf("%d\n",num3);
+ 
+    
+    return 0;
+}
+
