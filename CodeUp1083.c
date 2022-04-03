b19ffe971e63918d206020f79db95af99722f994
PepsiZeroPpa <chinchackseojun@gmail.com>
2022-04-03 17:04:39 +0900

    CodeUp1083

---
+++ CodeUp1083.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (void){
+	
+	int i;
+	scanf("%d",&i);
+	
+	for(int j=1;j<=i;j++){
+		
+		if(j%3==0){
+			
+			printf("X ");
+		}
+		else{
+			printf("%d ",j);
+		}
+	}
+	
+	return 0;
+	
+}
